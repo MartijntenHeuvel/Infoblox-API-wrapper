5e50d804c44472b3c27f4c8dc9cbc49e3b0f1ff7
Martijn ten Heuvel <mtenheuv@redhat.com>
2017-06-23 11:55:48 +0200

    added a main function

--- call-to-infoblox.py
+++ call-to-infoblox.py
@@ -8,46 +8,48 @@ import argparse
 sys.path.insert(0, 'Infoblox-API-Python/')
 import infoblox
 
-parser = argparse.ArgumentParser()
-
 # insert the ip here if this is static
 infobloxishere="192.168.0.1"
 
-parser.add_argument('-u', '--username',
-    help='name of user'
-)
-parser.add_argument('-p', '--password',
-    help='password'
-)
-parser.add_argument('-b', '--infobloxip',
-    help='ipadress of the infoblox device',
-    default=infobloxishere
-)
+def main():
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('-n', '--hostname',
-    help='new hostname'
-)
-parser.add_argument('-i', '--ipaddress',
-    help='new ipaddress'
-)
-parser.add_argument('-m', '--mode', 
-    help='mode: create or delete',
-    default='create'
-)
-
-args = parser.parse_args()
-
-
-iba_api = infoblox.Infoblox(args.infobloxip, args.username, args.password, '1.6', 'default', 'default')
-
-if args.mode == 'create':
-    print("Trying to create {host} with {ip}".format(
-        host=args.hostname,
-        ip=args.ipaddress)
+    parser.add_argument('-u', '--username',
+        help='name of user'
     )
-    call = iba_api.create_host_record(args.ipaddress, args.hostname)
-    print call
-else:
-    print("trying to delete {host}".format(host=args.hostname))
-    call = iba_api.delete_host_record(args.hostname)
-    print call
+    parser.add_argument('-p', '--password',
+        help='password'
+    )
+    parser.add_argument('-b', '--infobloxip',
+        help='ipadress of the infoblox device',
+        default=infobloxishere
+    )
+    parser.add_argument('-n', '--hostname',
+        help='new hostname'
+    )
+    parser.add_argument('-i', '--ipaddress',
+        help='new ipaddress'
+    )
+    parser.add_argument('-m', '--mode', 
+        help='mode: create or delete',
+        default='create'
+    )
+
+    args = parser.parse_args()
+
+    iba_api = infoblox.Infoblox(args.infobloxip, args.username, args.password, '1.6', 'default', 'default')
+
+    if args.mode == 'create':
+        print("Trying to create {host} with {ip}".format(
+            host=args.hostname,
+            ip=args.ipaddress)
+        )
+        call = iba_api.create_host_record(args.ipaddress, args.hostname)
+        print call
+    else:
+        print("trying to delete {host}".format(host=args.hostname))
+        call = iba_api.delete_host_record(args.hostname)
+        print call
+
+if __name__ == "__main__":
+    main()
